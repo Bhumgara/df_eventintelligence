8da4d771e49414f71955059bd4b64ed191d899d9
Bhumgara <54769848+Bhumgara@users.noreply.github.com>
2026-08-09 13:11:21 +0100

    quick mock of streamlit file structure

---
+++ streamlit_dashboard/pages/2_Population_Regions.py
@@ -0,0 +1,3 @@
+'''
+Are there UK regions where live music event density is low relative to their size or population?
+'''
---
+++ streamlit_dashboard/utility/api_handler.py
@@ -0,0 +1,44 @@
+from apis import ticketmaster as tck
+from data_processor.ticketmaster.venues_processor import VenuesProcessor
+from data_processor.ticketmaster.ticketmaster_processor import TicketmasterProcessor
+from data_processor.ticketmaster.events_processor import EventsProcessor
+
+def call_api():
+    endpoint = "/venues.json"
+    embedded_parts = [("_embedded", "venues")]
+    kword_filters_gb= {"countryCode":"GB"}
+    kword_filters_uk= {"countryCode":"UK"}
+    venues_list = []
+
+    response_data_gb = tck.get_ticketmaster_data(endpoint, embedded_parts, filters=kword_filters_gb)
+    response_data_uk = tck.get_ticketmaster_data(endpoint, embedded_parts, filters=kword_filters_uk)
+    venues_list = response_data_gb + response_data_uk
+
+    endpoint = "/events.json"
+    embedded_parts = [("_embedded", "events")]
+    kword_filters_gb= {"classificationName":"festival", "countryCode":"GB"}
+    kword_filters_uk= {"classificationName":"festival", "countryCode":"UK"}
+    events_list = []
+
+    response_data_gb = tck.get_ticketmaster_data(endpoint, embedded_parts, filters=kword_filters_gb)
+    response_data_uk = tck.get_ticketmaster_data(endpoint, embedded_parts, filters=kword_filters_uk)
+    events_list = response_data_gb + response_data_uk
+
+
+    venues_models, invalid_models = VenuesProcessor.validate_venues(venues_list)
+    venues_df = VenuesProcessor.build_venues_dataframe(venues_models)
+
+    events_models, invalid_events_models = EventsProcessor.validate_events(events_list)
+    events_df = EventsProcessor.build_events_dataframe(events_models)
+
+    vn_copy = venues_df.copy()
+    ev_copy = events_df.copy()
+
+    evp = EventsProcessor()
+    vnp = VenuesProcessor()
+
+
+    tmp = TicketmasterProcessor(ev_copy, vn_copy, evp, vnp)
+    merged_df = tmp.merge_venues_to_events()
+
+    return(merged_df)
